a4b86c9407b47cff48c4accdcabeb0aa78045ec9
Naufal Hasbialhaq <naufal.hasbialhaq18@gmail.com>
2026-05-23 20:00:03 +0700

    fix : ngilangin tulisan dashboard di paling atas

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -3,13 +3,12 @@ import { Stack, Tabs } from "expo-router";
 export default function RootLayout() {
   return (
     <Tabs initialRouteName="index">
-      <Tabs.Screen name="dashboard" options={{ title: "Dashboard" }} />
+      <Tabs.Screen name="dashboard" options={{ headerShown: false, title: "Dashboard" }} />
       <Tabs.Screen name="history" options={{ title: "History" }} />
       <Tabs.Screen name="index" options={{ headerShown: false, title: "Expense" }} />
       <Tabs.Screen name="planning" options={{ title: "Planning" }} />
       <Tabs.Screen name="setting" options={{ title: "Setting" }} />
     </Tabs>
   )
-  
 }
 
