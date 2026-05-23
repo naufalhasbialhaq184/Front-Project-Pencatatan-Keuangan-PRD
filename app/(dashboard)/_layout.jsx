3f3e5ed8eca53af5ce73cdf50110e6247ff0f36b
Naufal Hasbialhaq <naufal.hasbialhaq18@gmail.com>
2026-05-23 20:04:09 +0700

    feat : ngilangin tulisan history di paling atas

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
     <Tabs initialRouteName="index">
       <Tabs.Screen name="dashboard" options={{ title: "Dashboard" }} />
-      <Tabs.Screen name="history" options={{ title: "History" }} />
+      <Tabs.Screen name="history" options={{ headerShown: false,title: "History" }} />
       <Tabs.Screen name="index" options={{ headerShown: false, title: "Expense" }} />
       <Tabs.Screen name="planning" options={{ title: "Planning" }} />
       <Tabs.Screen name="setting" options={{ title: "Setting" }} />
